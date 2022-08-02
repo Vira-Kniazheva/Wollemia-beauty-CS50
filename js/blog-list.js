2411a015baf1db0cfb1222fefe37384c7ced2994
Vira Kniazheva <vira.kniazheva@gmail.com>
2022-08-02 19:14:33 +0300

    deleted teka structure. Create modal window on blog page

--- js/blog-list.js
+++ js/blog-list.js
@@ -1,21 +1,21 @@
 class BlogList {
-    constructor() {
-        this.container = document.querySelector('.blog-container');
-        this.BlogsService = new BlogsService();
-        this.renderBlogs();
-    }
-    async renderBlogs() {
-        let blogListDomString = '';
-        const blogs = await this.BlogsService.getBlogs();
-        blogs.forEach(blog => {
-            blogListDomString += this.createBlogDomString(blog);
-        });
-        this.container.innerHTML = blogListDomString;
-        //this.addEventListeners();
-    }
-    createBlogDomString(blog) {
-        return `<div class="col-lg-6 col-md-6">
-            <article class="blog-post">
+  constructor() {
+    this.container = document.querySelector('.blog-container');
+    this.blogsService = new BlogsService();
+    this.renderBlogs();
+  }
+  async renderBlogs() {
+    let blogListDomString = '';
+    const blogs = await this.blogsService.getBlogs();
+    blogs.forEach(blog => {
+      blogListDomString += this.createBlogDomString(blog);
+    });
+    this.container.innerHTML = blogListDomString;
+    this.addEventListeners();
+  }
+  createBlogDomString(blog) {
+    return `<div class="col-lg-6 col-md-6">
+            <article class="blog-post blog-top-2line">
               <img src="../img/${blog.image}" alt="${blog.name}" />
               <div class="date">
                 <div>
@@ -28,11 +28,23 @@ class BlogList {
               <p class="my-3 description">
                 ${blog.description}
               </p>
-              <a href="#" class="link-more read" data-id = ${blog.id}
+              <a href="#" class="link-more btn-read" data-bs-toggle="modal" data-bs-target="#blog-info-modal" data-id = ${blog.id}
                 >Читати більше <i class="ri-arrow-right-s-line icon"></i
               ></a>
             </article>
         </div>`;
-    }
+  }
+  addEventListeners() {
+    document.querySelectorAll('.btn-read').forEach(btn => {
+      btn.addEventListener('click', this.showBlogInfo.bind(this));
+    });
+  }
+  async showBlogInfo(event) {
+    const id = event.target.dataset.id;
+    const blog = await this.blogsService.getBlogById(id);
+    const modal = document.querySelector('#blog-info-modal');
+    modal.querySelector('.modal-title').innerHTML = blog.name;
+    modal.querySelector('.blog-description').innerHTML = blog.full_description;
+  }
 }
 new BlogList();
